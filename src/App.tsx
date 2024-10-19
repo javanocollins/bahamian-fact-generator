d02fdc682901f1d7e806508e17a2c09271d90e81
Javano Collins <jdcollins242@gmail.com>
2024-10-19 13:53:51 -0400

    updaate: cleanup; added lottie animation

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { useState } from "react";
 import "./App.css";
 
 import facts from "./assets/facts.json";
+import LottieAnimationJson from "./assets/ideas-generator.json";
+import Lottie from "lottie-react";
 
 const App = () => {
     const generateResponse = async () => {
@@ -9,20 +11,31 @@ const App = () => {
 
         const randomIndex = Math.floor(Math.random() * factsValues.length);
 
-        console.log(factsValues);
-
         setResponse(factsValues[randomIndex].fact);
     };
 
-    const [response, setResponse] = useState("Nothing Yet...");
+    const [response, setResponse] = useState<string | null>(null);
 
     return (
         <div className="flex flex-col items-center justify-center">
-            <h1 className="text-5xl font-bold mt-3 mb-10">
+            <Lottie
+                className="w-[300px] h-[300px]"
+                animationData={LottieAnimationJson}
+                height={100}
+                width={100}
+            />
+            <h1 className="text-3xl font-bold mt-3">
                 Random Bahamian Fact Generator
             </h1>
-            <p className="text-xl font-semibold mt-3">{response}</p>
-            <button className="mt-3" onClick={() => generateResponse()}>
+            {response && (
+                <p className="text-xl font-semibold mt-6 max-w-[600px] bg-[#ffffff4d] p-10 text-black rounded-xl">
+                    {response}
+                </p>
+            )}
+            <button
+                className="mt-6 bg-white text-black"
+                onClick={() => generateResponse()}
+            >
                 Generate Fact
             </button>
         </div>
