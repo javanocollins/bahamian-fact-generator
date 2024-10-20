4453c352d40a60e4ded02e71aa6d6b32142ed1dd
Javano Collins <jdcollins242@gmail.com>
2024-10-20 18:22:40 -0400

    misc

--- scripts/app_start.sh
+++ scripts/app_start.sh
@@ -3,8 +3,5 @@
 # Move to the project directory
 cd /home/ec2-user/bahamian-fact-generator || exit 1
 
-# Delete all running PM2 processes
-sudo pm2 delete all || echo "No PM2 processes to delete."
+sudo nginx -t && sudo systemctl reload nginx
 
-# Start the app with PM2
-sudo pm2 start "npm run dev" --name "bahamian-fact-generator"
