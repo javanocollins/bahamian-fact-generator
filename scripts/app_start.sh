03020e4b06b551aef2aa33e5252a3044a1ee638b
Javano Collins <jdcollins242@gmail.com>
2024-10-20 16:55:50 -0400

    misc

---
+++ scripts/app_start.sh
@@ -0,0 +1,2 @@
+pm2 delete all
+pm2 start "npm run dev" --name "bahamian-facts"
--- scripts/clean_build_directory.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -e
-exec > /var/log/clean_build_directory.log 2>&1
-
-sudo rm -rf /var/www/html/*
-echo "Cleaned build directory."
