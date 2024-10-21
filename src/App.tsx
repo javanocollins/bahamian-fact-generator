60d31781335835bcf4b4ed1a8a107de064e7090d
Javano Collins <jdcollins242@gmail.com>
2024-10-21 09:10:51 -0400

    misc

--- src/App.tsx
+++ src/App.tsx
@@ -19,20 +19,20 @@ const App = () => {
     return (
         <div className="flex flex-col items-center justify-center">
             <Lottie
-                className="w-[300px] h-[300px]"
+                className="w-[350px] h-[350px]"
                 animationData={LottieAnimationJson}
                 height={100}
                 width={100}
             />
             <h1 className="text-3xl font-bold mt-3">
-                Random Bahamian Fact Generator
+                Bahamian Fact Generator
             </h1>
             {response && (
                 <p className="text-xl font-semibold mt-6 max-w-[600px] bg-[#ffffff4d] p-10 text-black rounded-xl">
                     {response}
                 </p>
             )}
-            
+
             <button
                 className="mt-6 bg-white text-black"
                 onClick={() => generateResponse()}
